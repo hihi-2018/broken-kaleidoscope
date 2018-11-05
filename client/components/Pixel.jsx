c200c8d0746dcb39bc96f6d1498e6900f6e7aa82
Karry <hello@devacademy.co.nz>
2018-11-06 11:51:10 +1300

    dragEnter function w/Trump not working

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -13,6 +13,8 @@ class Pixel extends React.Component {
       style : { 
         height: '100px',
         width: '100px',
+        opacity: '1',
+        backgroundImage: '',
         backgroundColor: randomHexColor()
       }
     }
@@ -30,11 +32,59 @@ class Pixel extends React.Component {
     })
   }
   
+  doubleHandler = evt => {
+    console.log('doubleclick', evt)
+
+    let doubleStyle = {...this.state.style}
+    doubleStyle.backgroundColor = 'white'
+    this.setState({
+      style: doubleStyle
+    })
+  }
+
+  changeDimension = evt => {
+    console.log('change dimenson pass over', evt)
+
+    let changeDimensions = {...this.state.style}
+    changeDimensions.width = '200px'
+    changeDimensions.height = '200px'
+
+    this.setState({
+      style: changeDimensions
+    })
+  }
+
+  rightClick = evt => {
+    console.log('rightclick clicked',evt)
+    evt.preventDefault()
+
+    let changeContent = {...this.state.style}
+    changeContent.opacity = '0.3'
+
+    this.setState({
+      style: changeContent
+    })
+  }
+
+  // dragEnter = evt =>{
+  //   console.log('dragEnter shows images', evt)
+  //   evt.preventDefault()
+
+  //   let showImage = {...this.state.style}
+  //   showImage.backgroundImage = 'url("../trump.jpg")'
+
+  //   this.setState({
+  //     style: showImage
+  //   })
+  // }
+
+
+  
+
   render(){
     return(
       <div>
-      <button onClick={this.clickHandler}>Click</button>
-      <div style={this.state.style}></div>
+      <div onDragEnter={this.dragEnter} onContextMenu={this.rightClick} onClick={this.clickHandler} onDoubleClick={this.doubleHandler} onMouseEnter={this.changeDimension} style={this.state.style}></div>
       </div>
     )
   }
