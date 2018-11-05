6e546b4584868e3ed8f5d6e7ca4f343a69791d7f
Joan-SHI <qiongnashi@gmail.com>
2018-11-06 11:40:07 +1300

    change pixel to be a class

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 
 
-const Pixel = () =>{
-    return (
-        <div style={{
-            height:'50px',
-            width:'50px',
-            background:'blue'
-        }}>
+// const Pixel = () =>{
+//     return (
+//         <div style={{
+//             height:'50px',
+//             width:'50px',
+//             background:'blue'
+//         }}>
 
 
-        </div>
-    )
+//         </div>
+//     )
+// }
+
+
+class Pixel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            style: {
+                height: '50px',
+                width: '50px',
+                background: 'blue'
+            }
+        }
+    }
+
+    render() {
+        
+        return <div style={this.state.style}></div>
+    }
 }
-
 
 export default Pixel
