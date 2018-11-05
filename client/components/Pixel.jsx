127578b77b63a3334b7c4932a6a9effc63b83689
Garry <garry@garry.bar>
2018-11-06 10:56:57 +1300

    Got first pixel stateless component, have deleted server public

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Pixel (props) {
+  return (
+    <div style={{
+      fontFamily: 'Times New Roman',
+      height: '80px',
+      width: '80px',
+      backgroundColor: 'blue'
+
+    }}>
+
+    </div>
+
+  )
+}
+
+export default Pixel;
