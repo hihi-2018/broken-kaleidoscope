a725087ba2b4c76edb0581bc46d51c13862fe700
Garry <garry@garry.bar>
2018-11-06 11:17:52 +1300

    turned Pixel component into stateful component and it all works

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,18 +1,76 @@
 import React from 'react'
 
-function Pixel (props) {
-  return (
-    <div style={{
-      fontFamily: 'Times New Roman',
-      height: '80px',
-      width: '80px',
-      backgroundColor: 'blue'
+// function Pixel (props) {
+//   return (
+//     <div style={{
+//       fontFamily: 'Times New Roman',
+//       height: '80px',
+//       width: '80px',
+//       backgroundColor: 'blue'
 
-    }}>
+//     }}>
 
-    </div>
+//     </div>
 
-  )
+//   )
+// }
+
+// The same thing as above, using different style for declaring our function
+// const Pixel = props => {
+//   return (
+//     <div style={{
+//       fontFamily: 'Times New Roman',
+//       height: '80px',
+//       width: '80px',
+//       backgroundColor: 'blue'
+
+//     }}>
+
+//     </div>
+
+//   )
+// }
+
+// and third way to create our function
+// const Pixel = function (props) {
+//   return (
+//     <div style={{
+//       fontFamily: 'Times New Roman',
+//       height: '80px',
+//       width: '80px',
+//       backgroundColor: 'blue'
+
+//     }}>
+//     </div>
+//   )
+// }
+
+class Pixel extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      style: {
+        fontFamily: 'Times New Roman',
+        height: '50px',
+        width: '50px',
+        backgroundColor: 'cornflowerblue'
+      }
+
+    }
+  }
+
+  // this.setState()
+
+  render(){
+    return (
+      <div style = {this.state.style}>
+      </div>
+    )
+  }
 }
+
+
+
 
 export default Pixel;
