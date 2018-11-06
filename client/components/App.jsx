781063dde914ac6000d6bad8f3579e669e40b390
Karry <hello@devacademy.co.nz>
2018-11-06 15:06:47 +1300

    fixed dev issue, page no longer crashes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,55 +2,7 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App =() => {
-  
-   return  Array.from({length:100000},() => <Pixel />)
-
-   /* [
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />,
-    <Pixel />
-
-
-
-  
-   
-] */
-
+   return  Array.from({length:10},() => <Pixel />)
 }
-}
- 
 
 export default App
